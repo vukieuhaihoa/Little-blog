7d36d327a9aa5224d5b4c7c9ead2eb66f07d7dfa
Hai Hoa <vukieuhaihoa@gmail.com>
2021-10-10 17:15:02 +0700

    add draftjs editor & auth page & user setting page

--- src/components/pages/posts/write/index.tsx
+++ src/components/pages/posts/write/index.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
-import clsx from 'clsx';
+import dynamic from 'next/dynamic';
 
+import { EditorState } from 'draft-js';
+// const Editor = dynamic(
+//   () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+//   { ssr: false }
+// );
+import { EditorProps } from 'react-draft-wysiwyg';
+
+const Editor = dynamic<EditorProps>(
+  () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+  { ssr: false }
+);
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'isomorphic-dompurify';
 // MUI imp
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
 
 const WriteComponent = () => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const [convertedContent, setConvertedContent] = useState<string>('');
+
+  const handleEditorChange = (state: any) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+
+  const convertContentToHTML = () => {
+    const currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
+
+  const createMarkup = (html: any) => {
+    return {
+      __html: DOMPurify.sanitize(html),
+    };
+  };
+
   return (
     <div className={styles['write-post-container']}>
       <div className={styles['upload-img']}>
@@ -27,14 +64,32 @@ const WriteComponent = () => {
           />
         </div>
         <div className={styles['post-form-group']}>
-          <textarea
+          {/* <textarea
             className={clsx(styles['write-input'], styles['write-text'])}
             placeholder="Tell your story..."
             autoFocus={true}
+          /> */}
+          <div className={styles['editor-and-preview']}>
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={handleEditorChange}
+            wrapperClassName={styles['editor-wrapper']}
+            editorClassName={styles['editor']}
+            placeholder="The message goes here..."
           />
+          <div
+            className={styles['preview']}
+            dangerouslySetInnerHTML={createMarkup(convertedContent)}
+          ></div>
+          </div>
+          
         </div>
         <div className={styles['post-form-group']}>
-          <Button className={styles['btn-submit']} variant="contained" color="primary">
+          <Button
+            className={styles['btn-submit']}
+            variant="contained"
+            color="primary"
+          >
             Publish
           </Button>
         </div>
